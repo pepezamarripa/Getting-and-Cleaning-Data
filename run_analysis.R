92d113882e1c0338120813173ec8d227ae876b22
pepezamarripa <pepe.zamarripa@gmail.com>
2023-05-12 15:53:25 -0600

    Commit with spaces and formatting

--- run_analysis.R
+++ run_analysis.R
@@ -10,53 +10,53 @@
 
 
 #Load packages
-install.packages("reshape2")
-install.packages("data.table")
-library("data.table")
-packages <- c("data.table", "reshape2")
-sapply(packages, require, character.only=TRUE, quietly=TRUE)
-path <- getwd()
+  install.packages("reshape2")
+  install.packages("data.table")
+  library("data.table")
+  packages <- c("data.table", "reshape2")
+  sapply(packages, require, character.only=TRUE, quietly=TRUE)
+  path <- getwd()
 
 #Get data
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(url, file.path(path, "dataFiles.zip"))
-unzip(zipfile = "dataFiles.zip")
+  url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(url, file.path(path, "dataFiles.zip"))
+  unzip(zipfile = "dataFiles.zip")
 
 
 # Load activity labels
-activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt"), col.names = c("classLabels", "activityName"))
+  activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt"), col.names = c("classLabels", "activityName"))
 
 #Load features and measurements
-features <- fread(file.path(path, "UCI HAR Dataset/features.txt"), col.names = c("index", "featureNames"))
-featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
-measurements <- features[featuresWanted, featureNames]
-measurements <- gsub('[()]', '', measurements)
+  features <- fread(file.path(path, "UCI HAR Dataset/features.txt"), col.names = c("index", "featureNames"))
+  featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
+  measurements <- features[featuresWanted, featureNames]
+  measurements <- gsub('[()]', '', measurements)
 
 #Load train data sets
-train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
-data.table::setnames(train, colnames(train), measurements)
-trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt"), col.names = c("Activity"))
-trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt"), col.names = c("SubjectNum"))
-train <- cbind(trainSubjects, trainActivities, train)
+  train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
+  data.table::setnames(train, colnames(train), measurements)
+  trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt"), col.names = c("Activity"))
+  trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt"), col.names = c("SubjectNum"))
+  train <- cbind(trainSubjects, trainActivities, train)
 
 # Load test datasets
-test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
-data.table::setnames(test, colnames(test), measurements)
-testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt"), col.names = c("Activity"))
-testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt"), col.names = c("SubjectNum"))
-test <- cbind(testSubjects, testActivities, test)
+  test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
+  data.table::setnames(test, colnames(test), measurements)
+  testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt"), col.names = c("Activity"))
+  testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt"), col.names = c("SubjectNum"))
+  test <- cbind(testSubjects, testActivities, test)
 
 # Merge train and test datasets
-train_test <- rbind(train, test)
+  train_test <- rbind(train, test)
 
 # Use descriptive names for activities
-train_test[["Activity"]] <- factor(train_test[, Activity], levels = activityLabels[["classLabels"]], labels = activityLabels[["activityName"]])
+  train_test[["Activity"]] <- factor(train_test[, Activity], levels = activityLabels[["classLabels"]], labels = activityLabels[["activityName"]])
 
 # Get the mean of each SubjectNum and Activity.
-train_test[["SubjectNum"]] <- as.factor(train_test[, SubjectNum])
-train_test <- reshape2::melt(data = train_test, id = c("SubjectNum", "Activity"))
-train_test <- reshape2::dcast(data = train_test, SubjectNum + Activity ~ variable, fun.aggregate = mean)
+  train_test[["SubjectNum"]] <- as.factor(train_test[, SubjectNum])
+  train_test <- reshape2::melt(data = train_test, id = c("SubjectNum", "Activity"))
+  train_test <- reshape2::dcast(data = train_test, SubjectNum + Activity ~ variable, fun.aggregate = mean)
 
 # Create the tidy table
-data.table::fwrite(x = train_test, file = "tidyData.txt", quote = FALSE)
+  data.table::fwrite(x = train_test, file = "tidyDataset.txt", quote = FALSE)
 
